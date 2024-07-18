118b1af86d6c407ff45b948379f1fd65a8722af1
Aliaksei Bedonik <c_aliaksei.bedonik@harness.io>
2024-07-18 17:23:43 +0200

    add more examples

--- main.tf
+++ main.tf
@@ -2,10 +2,40 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_s3_bucket" "ab_18_07_2024_terraformer_test_1" {
-  bucket  = "my-unique-bucket-name"
-  tags    = {
-	Name          = "ab-18-07-2024-terraformer-test-1"
-	Environment    = "Test"
+terraform {
+  backend "s3" {
+    bucket = "ab-18-07-2024-terraformer-state-test-1"
+    key    = "terraform.tfstate"
+    region = "us-west-2"
   }
+}
+
+resource "aws_ssm_parameter" "ab_18_07_2024_terraformer_test_1" {
+  name  = "ab_18_07_2024_terraformer_test_1"
+  type  = "String"
+  value = "This is a test parameter"
+}
+
+resource "aws_ssm_parameter" "ab_18_07_2024_terraformer_test_2" {
+  name  = "ab_18_07_2024_terraformer_test_2"
+  type  = "String"
+  value = "This is a test parameter"
+}
+
+resource "aws_ssm_parameter" "ab_18_07_2024_terraformer_test_3" {
+  name  = "ab_18_07_2024_terraformer_test_3"
+  type  = "String"
+  value = "This is a test parameter"
+}
+
+output "ab_18_07_2024_terraformer_test_1" {
+  value = aws_ssm_parameter.ab_18_07_2024_terraformer_test_1.arn
+}
+
+output "ab_18_07_2024_terraformer_test_2" {
+  value = aws_ssm_parameter.ab_18_07_2024_terraformer_test_2.arn
+}
+
+output "ab_18_07_2024_terraformer_test_3" {
+  value = aws_ssm_parameter.ab_18_07_2024_terraformer_test_3.arn
 }
